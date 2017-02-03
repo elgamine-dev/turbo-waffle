d69d4c9b1558a4b11c81645ce7c1548484906405
Amine El Ghayate <mhwebdev@gmail.com>
2017-02-03 09:24:18 +0100

    modif lien

--- src/Views/details.php
+++ src/Views/details.php
@@ -6,10 +6,14 @@
 </head>
 <body>
 	<div>
-		<a href="/">Liste de chaussettes</a>
+		<a href="/">Liste de chaussettes</a> - 
+		<a href="/?action=edit&id=<?= $sock->id ?>">Modifier Entrée</a>
 	</div>
 	<h3>Les chaussettes n°<?= $sock->id; ?>
 	sont de couleur <?= $sock->couleur; ?> et une pointure de <?= $sock->pointure; ?>.
 	La temperature de lavage conseillée est <?= $sock->temp_lavage; ?>°C et ont été lavées pour la dernière fois <?= $sock->date_lavage; ?></h3>
+	<div>
+		
+	</div>
 </body>
 </html>
